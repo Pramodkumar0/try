fcaba2cae3fcd11cd803eafabc5fbca2feae00f9
Pramodkumar0 <109613194+Pramodkumar0@users.noreply.github.com>
2022-10-01 13:59:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// let object = []
+//        function execute(){
+//         let innertext = document.getElementById("text").value
+//         let temobj={
+//             id : Date.now(),
+//             name : innertext
+//         }
+//         object.push(temobj);
+//        console.log(object)
+//         down();
+
+//        }
+       
+//        function down()
+//        {
+//         let element = document.createElement('div')
+//         element.setAttribute('class','child');
+//         let parent = document.getElementById("parent")
+//         parent.appendChild(element);
+//         element.innerText = object[object.length-1].name
+      
+
+//        }
+let tasklist = [];
+const execute = ()=>{
+    const name = document.getElementById("text").value;
+    console.log(name)
+
+}
